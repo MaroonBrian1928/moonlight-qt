a3d4494bd9faabd3c867da6918d92a490db95a5d
Popbones <popbones@gmail.com>
2026-04-04 00:37:52 +1100

    audio: make SDL queue threshold configurable

--- app/settings/streamingpreferences.h
+++ app/settings/streamingpreferences.h
@@ -182,6 +182,7 @@ public:
     Q_PROPERTY(bool reverseScrollDirection MEMBER reverseScrollDirection NOTIFY reverseScrollDirectionChanged)
     Q_PROPERTY(bool swapFaceButtons MEMBER swapFaceButtons NOTIFY swapFaceButtonsChanged)
     Q_PROPERTY(bool keepAwake MEMBER keepAwake NOTIFY keepAwakeChanged)
+    Q_PROPERTY(int audioQueueThresholdMs MEMBER audioQueueThresholdMs NOTIFY audioQueueThresholdMsChanged)
     Q_PROPERTY(CaptureSysKeysMode captureSysKeysMode MEMBER captureSysKeysMode NOTIFY captureSysKeysModeChanged)
     Q_PROPERTY(Language language MEMBER language NOTIFY languageChanged)
     Q_PROPERTY(AudioRenderer audioRenderer MEMBER audioRenderer NOTIFY audioRendererChanged)
@@ -221,6 +222,7 @@ public:
     bool reverseScrollDirection;
     bool swapFaceButtons;
     bool keepAwake;
+    int audioQueueThresholdMs;
     int packetSize;
     AudioConfig audioConfig;
     SpatialAudioConfig spatialAudioConfig;
@@ -276,6 +278,7 @@ signals:
     void swapFaceButtonsChanged();
     void captureSysKeysModeChanged();
     void keepAwakeChanged();
+    void audioQueueThresholdMsChanged();
     void languageChanged();
     void audioRendererChanged();
     void rendererChanged();
@@ -291,4 +294,3 @@ private:
 
     QQmlEngine* m_QmlEngine;
 };
-
